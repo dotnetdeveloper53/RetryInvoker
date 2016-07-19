9882d5d2d20e90cd474f5556f37b031e5de04423
dotnetdeveloper53 <ben.chivers53@gmail.com>
2016-07-19 13:33:41 +0100

    RetryInvoker

---
+++ Invoker.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RetryInvoker
+{
+    public static class Invoker
+    {
+        public interface IInterval
+        {
+            void Sleep();
+        }
+
+        class Interval : IInterval
+        {
+            TimeSpan _interval;
+            public Interval(TimeSpan interval) { _interval = interval; }
+            public void Sleep()
+            {
+                Thread.Sleep(_interval);
+            }
+        }
+
+        public static bool TryInvoke<T>(Func<T> del, out T value, int attempts = 3)
+        {
+            IList<Exception> exceptions;
+            return TryInvoke<Exception, T>(del, out value, out exceptions, attempts);
+        }
+
+        public static bool TryInvoke<T>(Func<T> del, out T value, out IList<Exception> exceptions, int attempts = 3)
+        {
+            return TryInvoke<Exception, T>(del, out value, out exceptions, attempts);
+        }
+
+        public static bool TryInvoke(Action del, int attempts = 3, TimeSpan? retryInterval = null)
+        {
+            if (del == null) throw new ArgumentNullException("del");
+
+            IList<Exception> exceptions;
+            object value;
+            return TryInvoke<Exception, object>(() =>
+            {
+                del();
+                return null;
+            }, out value, out exceptions, attempts, retryInterval);
+        }
+
+        public static bool TryInvoke<TException>(Action del, out IList<Exception> exceptions, int attempts = 3)
+            where TException : Exception
+        {
+            object value;
+            return TryInvoke<TException, object>(() =>
+            {
+                del();
+                return null;
+            }, out value, out exceptions, attempts);
+        }
+
+        public static bool TryInvoke<TException, T>(Func<T> del, out T value, out IList<Exception> exceptions, int attempts = 3)
+            where TException : Exception
+        {
+            return TryInvoke<TException, T>(del, out value, out exceptions, attempts, (TimeSpan?)null);
+        }
+
+        public static bool TryInvoke<TException, T>(Func<T> del, out T value, out IList<Exception> exceptions, int attempts = 3, TimeSpan? retryInterval = null)
+            where TException : Exception
+        {
+            return TryInvoke<TException, T>(del, out value, out exceptions, attempts, retryInterval.HasValue ? new Interval(retryInterval.Value) : null);
+        }
+
+        public static bool TryInvoke<TException, T>(Func<T> del, out T value, out IList<Exception> exceptions, int attempts = 3, IInterval interval = null)
+            where TException : Exception
+        {
+            if (del == null) throw new ArgumentNullException("del");
+            if (attempts <= 0) throw new ArgumentOutOfRangeException("attempts");
+
+            exceptions = new List<Exception>();
+
+            for (int i = 0; i < attempts; i++)
+            {
+                try
+                {
+                    value = del();
+                    return true;
+                }
+                catch (TException ex)
+                {
+                    exceptions.Add(ex);
+                    if (interval != null)
+                        interval.Sleep();
+                }
+            }
+
+            value = default(T);
+            return false;
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace RetryInvoker.Tests
+{
+    [TestClass]
+    public class InvokeRetrierTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TryInvoke_NullActionDelegate_ThrowsArgumentNullException()
+        {
+            Invoker.TryInvoke(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TryInvoke_NullFuncDelegate_ThrowsArgumentNullException()
+        {
+            int value;
+            Invoker.TryInvoke<int>(null, out value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TryInvoke_AttemptsEqualToZero_ThrowsArgumentOutOfRangeException()
+        {
+            Invoker.TryInvoke(() => { }, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TryInvoke_AttemptsLessThanZero_ThrowsArgumentOutOfRangeException()
+        {
+            Invoker.TryInvoke(() => { }, -1);
+        }
+
+        [TestMethod]
+        public void TryInvoke_DelAlwaysFails_ReturnsFalse()
+        {
+            IList<Exception> exs;
+            bool ret = Invoker.TryInvoke<Exception>(() => { throw new Exception(); }, out exs, 1);
+
+            Assert.IsFalse(ret);
+        }
+
+        static int i = 0;
+        [TestMethod]
+        public void TryInvoke_FailsFirstPassesSecond_ReturnsTrue()
+        {
+            IList<Exception> exs;
+            bool ret = Invoker.TryInvoke<Exception>(() => { if (i++ == 0) throw new Exception(); }, out exs, 2);
+
+            Assert.IsTrue(ret);
+            Assert.IsNotNull(exs);
+            Assert.AreEqual(1, exs.Count); //one exception for the first failed attempt
+        }
+
+        static int j = 0;
+        [TestMethod]
+        public void TryInvoke_DelThrowsExceptionAndAlwaysFails_ReturnsFalseAndProvidesListOfExceptions()
+        {
+            IList<Exception> exs;
+            bool ret = Invoker.TryInvoke<Exception>(() => { j++;  throw new Exception(); }, out exs, 3);
+
+            Assert.IsFalse(ret);
+            Assert.IsNotNull(exs);
+            Assert.AreEqual(3, exs.Count);  //3 failed attempts therefore should contain exceptions
+            Assert.AreEqual(3, j);  //check delegate is called 3 times
+        }
+
+
+        [TestMethod]
+        public void TryInvoke_DelThrowsUnhandledException_FailsSubsequentAttemptsAndExpectsException()
+        {
+            IList<Exception> exs = null;
+            bool ret = false;
+            try
+            {
+                ret = Invoker.TryInvoke<ArgumentOutOfRangeException>(() => { throw new ArgumentNullException(); }, out exs, 3);
+            }
+            catch (ArgumentNullException) { }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+            Assert.IsFalse(ret);
+            Assert.IsNotNull(exs);
+            Assert.AreEqual(0, exs.Count);
+
+        }
+
+        class MockSleeper : Invoker.IInterval
+        {
+            public int SleepMethodCount;
+
+            public void Sleep()
+            {
+                this.SleepMethodCount++;
+            }
+        }
+
+        [TestMethod]
+        public void TryInvoke_DelFails_SleeperIsCalledExpectedTimes()
+        {
+            MockSleeper mockSleeper = new MockSleeper();
+            IList<Exception> exs;
+            object value;
+            Invoker.TryInvoke<Exception, object>(() => { throw new Exception(); return null; }, out value, out exs, 3, mockSleeper);
+
+            Assert.AreEqual(3, mockSleeper.SleepMethodCount);
+        }
+    }
+}
